fe31950f3992d5d00e5bfbbb201173694d01281e
KRoss <kross@croc.ru>
2021-02-24 20:28:10 +0500

    Start BinarySearch project

---
+++ oop_hw3/BinarySearchTree/BinarySearchTree/BinaryTree/BinaryTreeDictionary.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace BinarySearchTree.BinaryTree
+{
+    class BinaryTreeDictionary<Tkey, Tvalue> : IDictionary<Tkey, Tvalue>
+    {
+
+        public BinaryTreeDictionary()
+        {
+        }
+
+        public Tvalue this[Tkey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ICollection<Tkey> Keys => throw new NotImplementedException();
+
+        public ICollection<Tvalue> Values => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(KeyValuePair<Tkey, Tvalue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<Tkey, Tvalue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(Tkey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<Tkey, Tvalue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<Tkey, Tvalue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Tkey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<Tkey, Tvalue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(Tkey key, [MaybeNullWhen(false)] out Tvalue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
